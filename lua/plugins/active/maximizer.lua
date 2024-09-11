c7856345aee6bced6eb0d82ddac8659c7256dce1
Manny Fay <mannythefay@gmail.com>
2024-09-11 10:54:48 +0200

    Update Maximizer plugin.

--- lua/plugins/active/maximizer.lua
+++ lua/plugins/active/maximizer.lua
@@ -1,6 +1,16 @@
+-------------------------------------------------------------------------------
+-- Maximizer Plugin
+-- https://github.com/declancm/maximize.nvim
+-------------------------------------------------------------------------------
+
 return {
-  "szw/vim-maximizer",
-  keys = {
-    { "<leader>m", "<cmd>MaximizerToggle<CR>", desc = "Maximize/minimize a split" },
-  },
+  'declancm/maximize.nvim',
+  config = true,
+
+
+  ---------------------------------------------------------------------------
+  ----- Key Mappings
+
+  vim.keymap.set("n", "<leader>m", "<cmd>Maximize<CR>"),  -- Toggle minimize/maximize a split.
 }
+
