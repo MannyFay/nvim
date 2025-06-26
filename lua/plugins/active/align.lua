26451be1122f967887b34cdd083df7296f6929de
MannyFay <mannythefay@gmail.com>
2025-06-26 20:34:11 +0200

    Revert on old init.lua.

--- lua/plugins/active/align.lua
+++ lua/plugins/active/align.lua
@@ -15,19 +15,21 @@ return {
     ---------------------------------------------------------------------------
     --- Key Mappings
 
--- INFO: This is not the problem of extra right cursor move:
-    -- vim.keymap.set( 'x', 'aoc',  function() require'align'.align_to_char({ length = 1, }) end, NS)                                                -- Align to 1 character.
-    -- vim.keymap.set( 'x', 'aocc', function() require'align'.align_to_char({ length = 2, }) end, NS)                                               -- Align to 2 characters.
-    -- vim.keymap.set( 'x', 'aos',  function() require'align'.align_to_string({ regex = false, }) end, NS)                                           -- Align to a string.
-    -- vim.keymap.set( 'x', 'aop',  function() require'align'.align_to_string({ regex = true, }) end, NS)                                            -- Align to a regex.
+    -- Visual mode alignments (select text first, then use keymap):
+    vim.keymap.set('x', '<leader>a=', function() require'align'.align_to_char({length = 1}) end, NS)           -- Align to = (variables, assignments)
+    vim.keymap.set('x', '<leader>a:', function() require'align'.align_to_char({length = 1}) end, NS)           -- Align to : (JSON, tables)
+    vim.keymap.set('x', '<leader>a,', function() require'align'.align_to_char({length = 1}) end, NS)           -- Align to , (function params)
+    vim.keymap.set('x', '<leader>a-', function() require'align'.align_to_string({regex = false}) end, NS)      -- Align to -- (comments)
+    vim.keymap.set('x', '<leader>a|', function() require'align'.align_to_char({length = 1}) end, NS)           -- Align to | (tables, markdown)
 
+    -- Interactive alignments with preview:
+    vim.keymap.set('x', '<leader>ac', function() require'align'.align_to_char({preview = true, length = 1}) end, NS)      -- Align to any character (interactive)
+    vim.keymap.set('x', '<leader>as', function() require'align'.align_to_string({preview = true, regex = false}) end, NS) -- Align to any string (interactive)
+    vim.keymap.set('x', '<leader>ar', function() require'align'.align_to_string({preview = true, regex = true}) end, NS)  -- Align to regex pattern (interactive)
 
-    -- Original plugin key mappings:
-    -- vim.keymap.set( 'x', 'aa', function() require'align'.align_to_char({ length = 1, }) end, NS)                                                -- Aligns to 1 character.
-    -- vim.keymap.set( 'x', 'ad', function() require'align'.align_to_char({ preview = true, length = 2, }) end, NS)                                -- Aligns to 2 characters with previews.
-    -- vim.keymap.set( 'x', 'aw', function() require'align'.align_to_string({ preview = true, regex = false, }) end, NS)                           -- Aligns to a string with previews.
-    -- vim.keymap.set( 'x', 'ar', function() require'align'.align_to_string({ preview = true, regex = true, }) end, NS)                            -- Aligns to a Vim regex with previews.
-    -- vim.keymap.set( 'n', 'gaw', function() local a = require'align' a.operator( a.align_to_string, { regex = false, preview = true, }) end, NS) -- Example gawip to align a paragraph to a string with previews.
-    -- vim.keymap.set( 'n', 'gaa', function() local a = require'align' a.operator(a.align_to_char) end, NS)                                        -- Example gaaip to align a paragraph to 1 character.
+    -- Normal mode operators (work with text objects like gaw"ip" for paragraph):
+    vim.keymap.set('n', 'ga=', function() local a = require'align' a.operator(function() a.align_to_char({length = 1}) end) end, NS) -- ga=ip to align paragraph to =
+    vim.keymap.set('n', 'gac', function() local a = require'align' a.operator(a.align_to_char) end, NS)                            -- gacip to align paragraph to character
   end
 }
+
