62597ddaa01e0201e83d4275bd7837339a8bab67
MannyFay <mannythefay@gmail.com>
2025-06-27 08:57:56 +0200

    Reformatting of lspconfig.lua.

--- lua/plugins/active/lsp/lspconfig.lua
+++ lua/plugins/active/lsp/lspconfig.lua
@@ -15,22 +15,22 @@ return {
     { "folke/neodev.nvim", opts = {} },
     "artemave/workspace-diagnostics.nvim",
   },
-  opts = {
-    event = "BufReadPre",
-  },
+
   config = function()
     local lspconfig = require("lspconfig")
     local mason_lspconfig = require("mason-lspconfig")
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
+    ---------------------------------------------------------------------------
+    -- LSP Attach Keymaps
     vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
       callback = function(ev)
         local keymap = vim.keymap
         local opts = { buffer = ev.buf, silent = true }
 
-        -- Navigation:
+        -- Navigation
         opts.desc = "Show LSP references"
         keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
         opts.desc = "Go to declaration"
@@ -42,13 +42,13 @@ return {
         opts.desc = "Show LSP type definitions"
         keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
-        -- Code actions:
+        -- Code actions
         opts.desc = "See available code actions"
         keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
         opts.desc = "Smart rename"
         keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-        -- Diagnostics:
+        -- Diagnostics
         opts.desc = "Show buffer diagnostics"
         keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
         opts.desc = "Show line diagnostics"
@@ -58,7 +58,7 @@ return {
         opts.desc = "Go to next diagnostic"
         keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end, opts)
 
-        -- Help and documentation:
+        -- Help and documentation
         opts.desc = "Show documentation for what is under cursor"
         keymap.set("n", "K", vim.lsp.buf.hover, opts)
         opts.desc = "Restart LSP"
@@ -90,12 +90,28 @@ return {
       virtual_text = {
         spacing = 4,
       },
+      underline = {
+        [vim.diagnostic.severity.ERROR] = true,
+        [vim.diagnostic.severity.WARN] = true,
+        [vim.diagnostic.severity.INFO] = true,
+        [vim.diagnostic.severity.HINT] = true,
+      },
+      update_in_insert = false,
+      severity_sort = true,
+      float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = true,
+        header = "",
+        prefix = "",
+      },
     })
 
 
 
     ---------------------------------------------------------------------------
-    -- Helpers
+    -- Helper function for workspace diagnostics
 
     local function setup_workspace_diagnostics(client, bufnr)
       if client.name ~= "copilot" then
@@ -106,149 +122,159 @@ return {
 
 
     ---------------------------------------------------------------------------
-    -- Setup LSP servers with Mason and LSPConfig
+    -- Mason LSP Setup Handlers
 
     if mason_lspconfig.setup_handlers then
       mason_lspconfig.setup_handlers({
 
-      -- Default handler for all LSP servers:
-      function(server_name)
-        lspconfig[server_name].setup({
-          capabilities = capabilities,
-          on_attach = setup_workspace_diagnostics,
-        })
-      end,
+        -- Default handler for all LSP servers:
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+            on_attach = setup_workspace_diagnostics,
+          })
+        end,
 
-      ["tailwindcss"] = function()
-        lspconfig["tailwindcss"].setup({
-          capabilities = capabilities,
-          on_attach = setup_workspace_diagnostics,
-          filetypes = {
-            "html", "css", "scss", "javascript", "javascriptreact",
-            "typescript", "typescriptreact", "svelte"
-          },
-          settings = {
-            tailwindCSS = {
-              experimental = {
-                classRegex = {
-                  { "([\"'`][^\"'`]*.*?[\"'`])", "[\"'`]([^\"'`]*)" }
+        -- Tailwind CSS:
+        ["tailwindcss"] = function()
+          lspconfig["tailwindcss"].setup({
+            capabilities = capabilities,
+            on_attach = setup_workspace_diagnostics,
+            filetypes = {
+              "html", "css", "scss", "javascript", "javascriptreact",
+              "typescript", "typescriptreact", "svelte"
+            },
+            settings = {
+              tailwindCSS = {
+                experimental = {
+                  classRegex = {
+                    { "([\"'`][^\"'`]*.*?[\"'`])", "[\"'`]([^\"'`]*)" }
+                  },
                 },
               },
             },
-          },
-        })
-      end,
+          })
+        end,
 
-      ["svelte"] = function()
-        lspconfig["svelte"].setup({
-          capabilities = capabilities,
-          on_attach = function(client, bufnr)
-            setup_workspace_diagnostics(client, bufnr)
-            vim.api.nvim_create_autocmd("BufWritePost", {
-              pattern = { "*.js", "*.ts" },
-              callback = function(ctx)
-                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-              end,
-            })
-          end,
-        })
-      end,
+        -- Svelte:
+        ["svelte"] = function()
+          lspconfig["svelte"].setup({
+            capabilities = capabilities,
+            on_attach = function(client, bufnr)
+              setup_workspace_diagnostics(client, bufnr)
+              vim.api.nvim_create_autocmd("BufWritePost", {
+                pattern = { "*.js", "*.ts" },
+                callback = function(ctx)
+                  client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+                end,
+              })
+            end,
+          })
+        end,
 
-      ["graphql"] = function()
-        lspconfig["graphql"].setup({
-          capabilities = capabilities,
-          on_attach = setup_workspace_diagnostics,
-          filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-        })
-      end,
+        -- GraphQL:
+        ["graphql"] = function()
+          lspconfig["graphql"].setup({
+            capabilities = capabilities,
+            on_attach = setup_workspace_diagnostics,
+            filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+          })
+        end,
 
-      ["emmet_ls"] = function()
-        lspconfig["emmet_ls"].setup({
-          capabilities = capabilities,
-          on_attach = setup_workspace_diagnostics,
-          filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-        })
-      end,
+        -- Emmet:
+        ["emmet_ls"] = function()
+          lspconfig["emmet_ls"].setup({
+            capabilities = capabilities,
+            on_attach = setup_workspace_diagnostics,
+            filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+          })
+        end,
 
-      ["lua_ls"] = function()
-        lspconfig["lua_ls"].setup({
-          capabilities = capabilities,
-          on_attach = setup_workspace_diagnostics,
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { "vim" },
-                unusedLocalExclude = { "_*" },
-              },
-              completion = { callSnippet = "Replace" },
-              workspace = {
-                checkThirdParty = false,
-              },
-              telemetry = { enable = false },
-            },
-          },
-        })
-      end,
-
-      ["intelephense"] = function()
-        lspconfig["intelephense"].setup({
-          capabilities = capabilities,
-          on_attach = setup_workspace_diagnostics,
-          filetypes = { "php", "blade" },
-          settings = {
-            intelephense = {
-              filetypes = { "php", "blade" },
-              files = {
-                associations = { "*.php", "*.blade.php" },
-                maxSize = 5000000,
+        -- Lua:
+        ["lua_ls"] = function()
+          lspconfig["lua_ls"].setup({
+            capabilities = capabilities,
+            on_attach = setup_workspace_diagnostics,
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" },
+                  unusedLocalExclude = { "_*" },
+                },
+                completion = { callSnippet = "Replace" },
+                workspace = {
+                  checkThirdParty = false,
+                },
+                telemetry = { enable = false },
               },
             },
-          },
-        })
-      end,
+          })
+        end,
 
-      ["cssls"] = function()
-        lspconfig["cssls"].setup({
-          capabilities = capabilities,
-          on_attach = setup_workspace_diagnostics,
-          filetypes = { "css", "scss", "less", "sass" },
-          settings = {
-            css = { validate = true, lint = { unknownAtRules = "ignore" } },
-            scss = { validate = true, lint = { unknownAtRules = "ignore" } },
-            less = { validate = true, lint = { unknownAtRules = "ignore" } },
-          },
-        })
-      end,
+        -- PHP:
+        ["intelephense"] = function()
+          lspconfig["intelephense"].setup({
+            capabilities = capabilities,
+            on_attach = setup_workspace_diagnostics,
+            filetypes = { "php", "blade" },
+            settings = {
+              intelephense = {
+                filetypes = { "php", "blade" },
+                files = {
+                  associations = { "*.php", "*.blade.php" },
+                  maxSize = 5000000,
+                },
+              },
+            },
+          })
+        end,
 
-      ["ansiblels"] = function()
-        lspconfig["ansiblels"].setup({
-          capabilities = capabilities,
-          on_attach = setup_workspace_diagnostics,
-          filetypes = { "yaml", "yml", "ansible" },
-        })
-      end,
+        -- CSS:
+        ["cssls"] = function()
+          lspconfig["cssls"].setup({
+            capabilities = capabilities,
+            on_attach = setup_workspace_diagnostics,
+            filetypes = { "css", "scss", "less", "sass" },
+            settings = {
+              css = { validate = true, lint = { unknownAtRules = "ignore" } },
+              scss = { validate = true, lint = { unknownAtRules = "ignore" } },
+              less = { validate = true, lint = { unknownAtRules = "ignore" } },
+            },
+          })
+        end,
 
-      ["yamlls"] = function()
-        lspconfig["yamlls"].setup({
-          capabilities = capabilities,
-          on_attach = setup_workspace_diagnostics,
-          filetypes = { "yaml", "yml" },
-        })
-      end,
+        -- Ansible:
+        ["ansiblels"] = function()
+          lspconfig["ansiblels"].setup({
+            capabilities = capabilities,
+            on_attach = setup_workspace_diagnostics,
+            filetypes = { "yaml", "yml", "ansible" },
+          })
+        end,
 
-      ["bashls"] = function()
-        lspconfig["bashls"].setup({
-          capabilities = capabilities,
-          on_attach = setup_workspace_diagnostics,
-          filetypes = { "sh", "bash", "zsh", "fish", "dash", "ksh" },
-        })
-      end,
-    })
-  end
+        -- YAML:
+        ["yamlls"] = function()
+          lspconfig["yamlls"].setup({
+            capabilities = capabilities,
+            on_attach = setup_workspace_diagnostics,
+            filetypes = { "yaml", "yml" },
+          })
+        end,
 
+        -- Bash:
+        ["bashls"] = function()
+          lspconfig["bashls"].setup({
+            capabilities = capabilities,
+            on_attach = setup_workspace_diagnostics,
+            filetypes = { "sh", "bash", "zsh", "fish", "dash", "ksh" },
+          })
+        end,
+      })
+    end
 
     -- ESLint has always to be outside of Mason handlers:
     lspconfig.eslint.setup({
+      capabilities = capabilities,
       settings = { packageManager = "yarn" },
       on_attach = function(client, bufnr)
         setup_workspace_diagnostics(client, bufnr)
